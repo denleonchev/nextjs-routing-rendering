795ab4d55ac75b184e5530f9327793bfddf341a4
den.leonchev@gmal.com <den.leonchev@gmail.com>
2025-11-14 12:06:15 +0000

    Add content to the news page

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,20 +1,23 @@
 import Link from "next/link";
 import styles from "@/app/news/page.module.css";
+import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function NewsPage() {
   return (
     <>
-      <h1>News page</h1>
+      <h1 className={styles.newsTitle}>News page</h1>
       <ul className={styles.newsList}>
-        <li>
-          <Link href="/news/first-news">First News Item</Link>
-        </li>
-        <li>
-          <Link href="/news/second-news">Second News Item</Link>
-        </li>
-        <li>
-          <Link href="/news/third-news">Third News Item</Link>
-        </li>
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
